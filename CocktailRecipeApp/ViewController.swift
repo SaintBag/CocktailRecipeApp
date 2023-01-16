389d8474bdf04c54c51ec064b94b7725997d17ca
Sebastian Worek <sebastian1.worek@gmail.com>
2023-01-16 10:52:01 +0100

    Initial Commit

---
+++ CocktailRecipeApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CocktailRecipeApp
+//
+//  Created by Sebastian on 16/01/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
